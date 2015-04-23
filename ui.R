038635ab8cdfdee1d2eb69c64ddca8996ee6fa62
gissi <gissi83@gmail.com>
2015-04-23 23:24:18 +0000

    adding files for coursera project

--- ui.R
+++ ui.R
@@ -1,19 +1,20 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  headerPanel("Interactive Shiny App to plot reggresion for EPL"),
+  headerPanel("Interactive Shiny App to plot reggresion"),
   sidebarPanel(
-    sliderInput("slider1", label = h3("number of players in reggresion"), min = 1, max = 695, value = 350, step = 5),
-    h4("your choosen number of players is:"),
+    sliderInput("slider1", label = h3("number of observations in reggresion"), min = 1, max = 150, value = 75, step = 5),
+    h4("your choosen number of observation is:"),
     verbatimTextOutput("inputValue"),
-    sliderInput("num", label = h3("select number of players to display data table for"), min = 1, max = 25, value = 1, step = 1)
+    sliderInput("num", label = h3("select number of players to display data table for"), min = 1, max = 10, value = 1, step = 1)
   ),
   mainPanel(
-    h3('Fantasy Premier league linear regression'),
+    h3('built in data linear regression'),
     
     plotOutput('newHist'),
     tableOutput('data'),
-    helpText(   a("See more on premier league fantasy side", href="http://fantasy.premierleague.com/") )
+    helpText(   a("For the code for this shiny app click here", href="https://github.com/Gissi/shiny_app_fantasy/tree/master") ),
+    helpText(   a("For the code for the data I wanted to display and worked locally but not when uploaded to shinyapp.io click here", href="https://github.com/Gissi/shiny_app_fantasy/tree/master") )
     
   )
   
